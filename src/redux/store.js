858eb72f44348a1418139c960fcbc63b61639c7d
hongsoom <hongsoom@kakao.com>
2022-06-28 09:39:39 +0900

    a basic framework

---
+++ src/redux/store.js
@@ -0,0 +1,19 @@
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import user from "./module/user";
+import post from "./module/post";
+import comment from "./module/comment";
+
+const rootReducer = combineReducers({
+  user: user,
+  post: post,
+  comment: comment,
+});
+
+const middlewares = [thunk];
+
+const enhancer = applyMiddleware(...middlewares);
+
+let store = createStore(rootReducer, enhancer);
+
+export default store();
