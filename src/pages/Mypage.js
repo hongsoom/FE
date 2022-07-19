e1df29556bcb1958bac8f49957c07a88611fe64c
hongsoom <hongsoom@kakao.com>
2022-07-19 23:59:26 +0900

    my page conflict

--- src/pages/Mypage.js
+++ src/pages/Mypage.js
@@ -1,21 +1,21 @@
 import React, { useEffect, useState } from "react";
 import "../css/mypage.css";
 
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 
-import { userAction } from '../redux/module/user';
-import { getMypostDB, getMybookmarkDB } from '../redux/module/post';
+import { userAction } from "../redux/module/user";
+import { getMypostDB, getMybookmarkDB } from "../redux/module/post";
 
-import leftArrowBlack from "../assets/leftArrowBlack.png"
-import setup from "../assets/setup.png"
-import talkwhite from "../assets/talkwhite.png"
-import heartwhite from "../assets/heartwhite.png"
+import leftArrowBlack from "../assets/leftArrowBlack.png";
+import setup from "../assets/setup.png";
+import talkwhite from "../assets/talkwhite.png";
+import heartwhite from "../assets/heartwhite.png";
 
 const Mypage = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const [toggle, setToggle] = useState('myPosts');
+  const [toggle, setToggle] = useState("myPosts");
 
   const [page, setPage] = useState(0);
   const [size, setSize] = useState(0);
@@ -26,58 +26,53 @@ const Mypage = () => {
 
   // ----------------- 나의 정보 / 나의 게시글 가져오기
 
+  useEffect(() => {
+    dispatch(userAction.myInfoDB());
+  }, [dispatch]);
 
-  useEffect(()=>{
-    dispatch(userAction.myInfoDB())
-  },[dispatch])
+  useEffect(() => {
+    dispatch(getMypostDB(size, page, id, direction));
+    dispatch(getMybookmarkDB(size, page, id, direction));
+  }, []);
 
-  useEffect(()=>{
-    dispatch(getMypostDB(
-      size, page, id, direction
-    ))
-    dispatch(getMybookmarkDB(
-      size, page, id, direction
-    ))
-  },[])
-
- 
-  const myInfo = useSelector(state=>state.user.myinfo)
-  const myPosts = useSelector(state=>state.post.myposts)
-  const myMarks = useSelector(state=>state.post.mybookmarks)
-  console.log(myMarks)
+  const myInfo = useSelector((state) => state.user.myinfo);
+  const myPosts = useSelector((state) => state.post.myposts);
+  const myMarks = useSelector((state) => state.post.mybookmarks);
+  console.log(myMarks);
 
   // ---------------------------------------------------
 
-  
   const onClickLeftArrow = () => {
-    navigate('/')
-  }
-  
+    navigate("/");
+  };
+
   const onClickSetup = () => {
-    navigate('/setup')
-  }
-  
-  const isChecked = (e) =>{
-    if(e.target.checked){
-      setToggle(e.target.value)
+    navigate("/setup");
+  };
+
+  const isChecked = (e) => {
+    if (e.target.checked) {
+      setToggle(e.target.value);
     }
-  }
+  };
 
-
-    return (
-      <>
+  return (
+    <>
       {/* 헤더 */}
       <div className="mypageHeader">
         <div className="mypageHeaderItemsWrap">
-          <div className="leftArrowBlack"
-            onClick={onClickLeftArrow}>
-            <img src={leftArrowBlack} alt="leftArrow"/>
+          <div className="leftArrowBlack" onClick={onClickLeftArrow}>
+            <img src={leftArrowBlack} alt="leftArrow" />
           </div>
-          <div className="myPageTitle" onClick={()=>
-            console.log('dd')}
-            >마이페이지</div>
-          <img src={setup} className="setup" alt="환경설정"
-          onClick={onClickSetup}/>
+          <div className="myPageTitle" onClick={() => console.log("dd")}>
+            마이페이지
+          </div>
+          <img
+            src={setup}
+            className="setup"
+            alt="환경설정"
+            onClick={onClickSetup}
+          />
         </div>
       </div>
 
@@ -86,21 +81,17 @@ const Mypage = () => {
         <div className="myProfile">
           <div className="myProfilePic">
             <div className="myProfilePicCircle">
-              <img src={myInfo&&myInfo.userImgUrl} alt=""/>
+              <img src={myInfo && myInfo.userImgUrl} alt="" />
             </div>
           </div>
 
           <div className="myProfileTxt">
             <div className="myProfileTxtTitle">
-              <div className="myProfileName">
-                {myInfo&&myInfo.nickname}
-              </div>
-              <div className="myProfileLogout">
-                로그아웃
-              </div>
+              <div className="myProfileName">{myInfo && myInfo.nickname}</div>
+              <div className="myProfileLogout">로그아웃</div>
             </div>
             <div className="myProfileTxtDetail">
-              {myInfo&&myInfo.userInfo}
+              {myInfo && myInfo.userInfo}
             </div>
           </div>
         </div>
@@ -108,93 +99,86 @@ const Mypage = () => {
         {/* 게시글 */}
         <div className="myprofilePosts">
           <div className="myprofilePostsTitle">
-            <div className="myPostsTitle"
-            style={toggle === "myPosts" ? {borderBottom:'2px solid #8ACEFF', fontWeight:'600'}:{borderBottom:'2px solid transparent'}}
+            <div
+              className="myPostsTitle"
+              style={
+                toggle === "myPosts"
+                  ? { borderBottom: "2px solid #8ACEFF", fontWeight: "600" }
+                  : { borderBottom: "2px solid transparent" }
+              }
             >
-
-
-
               <input
                 type="radio"
                 name="myPost"
                 value="myBookmark"
                 id="myBookmark"
                 onChange={isChecked}
-
-              <input type="radio" name="myPost" value="myBookmark" id="myBookmark"
-              onChange={isChecked}
-
               />
               <label htmlFor="myBookmark">즐겨찾기</label>
             </div>
           </div>
 
-
-        <div className="postsNmarks">
-        {toggle === 'myPosts' ? 
-          <div className="myPosts">
-            <div className="postboxs">
-              {myPosts&&myPosts.myposts.map((v,i)=>{
-                return(
-                  <div className="postbox" key={i}
-                  style={{backgroundImage:`url(${v.imgUrl})`, backgroundPosition:'center' , backgroundSize:'cover'}}
-                  >
-                    <div className="txtArea">
-                      <div className="postBoxTitle">{v.title}</div>
-                      <div className="myPostDetail">
-                        <div className="myPostTag">
-                          <div>
-                          #{v.regionCategory}
-                          </div>
-                          {v.themeCategory.map((v,i)=>{
-                            return (
-                              <div key={i}>
-                              #{v.themeCategory}
+          <div className="postsNmarks">
+            {toggle === "myPosts" ? (
+              <div className="myPosts">
+                <div className="postboxs">
+                  {myPosts &&
+                    myPosts.myposts.map((v, i) => {
+                      return (
+                        <div
+                          className="postbox"
+                          key={i}
+                          style={{
+                            backgroundImage: `url(${v.imgUrl})`,
+                            backgroundPosition: "center",
+                            backgroundSize: "cover",
+                          }}
+                        >
+                          <div className="txtArea">
+                            <div className="postBoxTitle">{v.title}</div>
+                            <div className="myPostDetail">
+                              <div className="myPostTag">
+                                <div>#{v.regionCategory}</div>
+                                {v.themeCategory.map((v, i) => {
+                                  return <div key={i}>#{v.themeCategory}</div>;
+                                })}
                               </div>
-                            )
-                          })}
-                          
-                        </div>
-                        <div className="myPostIcons">
-                          <img src={talkwhite} alt='댓글 갯수'/>
-                          {v.commentCount}
-                          <img src={heartwhite} alt='댓글 갯수'/>
-                          {v.loveCount}
-                        </div>
-
-
-                      </div>
-                    </div>
-                  </div>
-                )
-              })}
-              
-              
-            </div>
-          </div>
-
-          :
-        
-
-          <div className="myBookmarks" id="myBookmarks">
-            <div className="postboxs">
-              {myMarks&&myMarks.mybookmarks.content.map((v,i)=>{
-                return(
-                  <div className="postbox" key={i}
-                  style={{backgroundImage:`url(${v.imgUrl})`, backgroundPosition:'center' , backgroundSize:'cover'}}
-                  >
-                    <div className="txtArea">
-                      <div className="postBoxTitle">{v.title}</div>
-                      <div className="myPostDetail">
-                        <div className="myPostTag">
-                          <div>
-                          #{v.regionCategory}
+                              <div className="myPostIcons">
+                                <img src={talkwhite} alt="댓글 갯수" />
+                                {v.commentCount}
+                                <img src={heartwhite} alt="댓글 갯수" />
+                                {v.loveCount}
+                              </div>
+                            </div>
                           </div>
-                          {v.themeCategory.map((v,i)=>{
-                            return (
-                              <div key={i}>
-                              #{v.themeCategory}
-
+                        </div>
+                      );
+                    })}
+                </div>
+              </div>
+            ) : (
+              <div className="myBookmarks" id="myBookmarks">
+                <div className="postboxs">
+                  {myMarks &&
+                    myMarks.mybookmarks.content.map((v, i) => {
+                      return (
+                        <div
+                          className="postbox"
+                          key={i}
+                          style={{
+                            backgroundImage: `url(${v.imgUrl})`,
+                            backgroundPosition: "center",
+                            backgroundSize: "cover",
+                          }}
+                        >
+                          <div className="txtArea">
+                            <div className="postBoxTitle">{v.title}</div>
+                            <div className="myPostDetail">
+                              <div className="myPostTag">
+                                <div>#{v.regionCategory}</div>
+                                {v.themeCategory.map((v, i) => {
+                                  return <div key={i}>#{v.themeCategory}</div>;
+                                })}
                               </div>
                               <div className="myPostIcons">
                                 <img src={talkwhite} alt="댓글 갯수" />
@@ -213,8 +197,8 @@ const Mypage = () => {
           </div>
         </div>
       </div>
-      </>
-  )
-}
+    </>
+  );
+};
 
 export default Mypage;
