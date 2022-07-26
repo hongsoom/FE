2e84efdb81066b6e7654b7e8c73b831c3c070072
hongsoom <hongsoom@kakao.com>
2022-07-26 13:21:49 +0900

    Feat : feat 카카오 소셜 로그인 기능 추가
    카카오 로그인 api를 통해 카카오 소셜 로그인 기능 추가

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
+import KakaoSocial from "./pages/KakaoSocial";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/oauth/kakao/callback" element={<KakaoSocial />} />
       </Routes>
     </div>
   );
