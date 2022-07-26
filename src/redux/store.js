28cd5c2c5d7ee9bb064247a983621fae7d1aa687
hongsoom <hongsoom@kakao.com>
2022-07-26 14:10:19 +0900

    Feat : feat 북마크, 좋아요 기능 추가
    북마크, 좋아요 기능을 main post 따로 filter post 따로 적용했습니다.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,10 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
+import post from "./module/post";
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+  post,
+});
 
 const middlewares = [thunk];
 
