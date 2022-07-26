f8376093827c73ce239ab4c9983521d420e2caf7
hongsoom <hongsoom@kakao.com>
2022-07-26 22:05:38 +0900

    Feat : feat 로그인 상태와 비로그인 상태 때 다른 헤더 모습

--- src/shared/Request.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-  baseURL: "http://sparta-hj.site:8080",
-  headers: { Authorization: localStorage.getItem("token") },
-});
-
-export default instance;
