ac1b645b87da0ae11ee9636f184e9cfc3806f28b
hongsoom <hongsoom@kakao.com>
2022-07-26 16:19:01 +0900

    Feat : feat 필터를 적용해서 필터 결과 값으로 정렬
    필터 버튼을 눌러서 누른 결과 값을 정렬해서 보여줍니다.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Filter from "./pages/Filter";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/filter/:keyword" element={<Filter />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
