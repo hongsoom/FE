47ebf14e264904cf85284b44891084cd8f7b565c
hongsoom <hongsoom@kakao.com>
2022-07-26 15:21:11 +0900

    Feat : feat 키워드 입력 기능 추가
    키워드를 입력하면 입력한 값에 따라 결과를 보여주는 것을 정렬했습니다.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Search from "./pages/Search";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/search/:keyword" element={<Search />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
