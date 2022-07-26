6bc523732a58b81dfcd7ed7bcf2979a18164ac04
hongsoom <hongsoom@kakao.com>
2022-07-26 21:24:51 +0900

    Fix : fix detail 경로 수정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Detail from "./pages/Detail";
 
 function App() {
-  return <div className="App"></div>;
+  if (window.Kakao) {
+    const kakao = window.Kakao;
+    if (!kakao.isInitialized()) {
+      kakao.init(`${process.env.REACT_APP_KAKAO_KEY}`);
+    }
+  }
+
+  return (
+    <div className="App">
+      <Route path="/detail/:id" element={<Detail />} />
+    </div>
+  );
 }
 
 export default App;
