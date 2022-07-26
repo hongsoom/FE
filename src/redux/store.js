67541ec9acc7bc2fe44126090662ba07c9669a0f
hongsoom <hongsoom@kakao.com>
2022-07-26 13:33:47 +0900

    Feat : feat 댓글 기능 추가
    댓글 기능 추가 및 css 작성

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,10 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
+import comment from "./module/comment";
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+  comment,
+});
 
 const middlewares = [thunk];
 
