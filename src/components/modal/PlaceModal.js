3b1bc80de08fbde42b2723aecfa00369341eae07
soojeongkimkr <sjk5689612@gmail.com>
2022-08-02 00:56:39 +0900

    Feat: feat 상세페이지 핀 포커스 시 인포윈도우 생성

--- src/components/modal/PlaceModal.js
+++ src/components/modal/PlaceModal.js
@@ -5,10 +5,10 @@ import "../../css/placeModal.scss"
 const { kakao } = window
 
 const PlaceModal = (props) => {
-  const {showPlaceModal, closePlaceModal, myInfo, select, setSelect, setFocus, myMap, checkAllFin} = props
+  const {showPlaceModal, closePlaceModal, myInfo, select, setFocus, myMap, checkAllFin} = props
   
   // 선택한 장소 핀찍기
-  const panTo= (place) =>{
+  const panTo= (place, list) =>{
     const options = {
       center: new kakao.maps.LatLng(37.5666805, 126.9784147),
       level: 4,
@@ -22,11 +22,39 @@ const PlaceModal = (props) => {
       position: markerPosition
   });
     marker.setMap(map);
+
+    for (var i = 0; i < list.length; i++) {
+      // 마커를 생성
+      const marker = new kakao.maps.Marker({
+        map: map, // 마커를 표시할 지도
+        position: new kakao.maps.LatLng(list[i].y, list[i].x),
+        // position: positions[i].latlng, // 마커를 표시할 위치
+        title: list[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+        place_name: list[i].place_name,
+      });
+
+      kakao.maps.event.addListener(marker, "click", function () {
+        var infowindow = new kakao.maps.InfoWindow({
+          zIndex: 1,
+          removable: true,
+        });
+        infowindow.setContent(
+          '<div style="padding:5px;font-size:12px;"> <b>' +
+            place.place_name +
+            "</b> <br/>" +
+            place.address_name +
+            "<br/>" +
+            place.phone +
+            "</div>"
+        );
+        infowindow.open(map, marker);
+      });
+    }
   }
  
   const onFocusPlaceHandler = (v) => {
     setFocus(v.place_name)
-    panTo(v)
+    panTo(v, select)
     closePlaceModal()
   }
 
