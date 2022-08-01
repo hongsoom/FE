32720f332ad7f052898c80e53c026f1902458e41
hongsoom <hongsoom@kakao.com>
2022-08-01 19:29:33 +0900

    Fix : fix 무한스크롤 모바일 버전

--- src/components/share/InfinityScroll.js
+++ src/components/share/InfinityScroll.js
@@ -22,8 +22,10 @@ const InfinityScroll = (props) => {
     }
 
     window.addEventListener("scroll", handleScroll);
+    window.addEventListener("touchmove", handleScroll);
     return () => {
       window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("touchmove", handleScroll);
     };
   }, [nextPage]);
 
