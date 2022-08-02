63096a56981f3a10299183098e2f74ce5bc4d5d7
hongsoom <hongsoom@kakao.com>
2022-08-02 18:43:31 +0900

    Style : style header 아이콘 변경

--- src/components/share/Header.js
+++ src/components/share/Header.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import RegionFilter from "../filter/RegionFilter";
 import "../../css/header.scss";
-import categoryMenu from "../../assets/categoryMenu.png";
+import info from "../../assets/info.png";
 import write from "../../assets/write.png";
 import mypage from "../../assets/mypage.png";
 import logoSmail from "../../assets/logo-smail.png";
@@ -12,26 +12,18 @@ const Header = () => {
 
   const is_login = localStorage.getItem("token") ? true : false;
 
-  const [viewCategory, setViewCategory] = useState(false);
-
-  const onClick = () => {
-    setViewCategory(!viewCategory);
-  };
-
   return (
     <>
-      {viewCategory ? <RegionFilter onClick={onClick} /> : null}
       <div className="header-container">
         <div className="header-content">
           <div className="header-image">
-            <img
-              src={categoryMenu}
-              alt="카테고리 보기"
-              className="categoryMenu-icon"
-              onClick={onClick}
-            />
+            <img src={info} alt="홈페이지 정보" className="categoryMenu-icon" />
             <div className="header-title" onClick={() => navigate("/")}>
-              <img src={logoSmail} alt="야너갈 로고" className="logoSmail-icon" />
+              <img
+                src={logoSmail}
+                alt="야너갈 로고"
+                className="logoSmail-icon"
+              />
               <p>야, 너도 갈래?</p>
             </div>
             <div className="header-icon">
