fa82a346bfd9e7434fe5568ee820616bcba54486
hongsoom <hongsoom@kakao.com>
2022-08-02 10:21:56 +0900

    Fix : fix lodash  add

--- src/components/share/InfinityScroll.js
+++ src/components/share/InfinityScroll.js
@@ -1,9 +1,10 @@
 import React, { useEffect } from "react";
+import _ from "lodash";
 
 const InfinityScroll = (props) => {
   const { children, callNext, lastPage, loading, nextPage } = props;
 
-  const _onScroll = (e) => {
+  const _onScroll = _.throttle((e) => {
     if (lastPage === true || nextPage === 0) {
       return;
     }
@@ -33,7 +34,7 @@ const InfinityScroll = (props) => {
       e.preventDefault();
       return;
     }
-  };
+  }, 1000);
 
   const onScroll = React.useCallback(_onScroll, [loading]);
 
